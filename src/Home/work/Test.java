f8928bf6e322d5937b72dd11d21231805f3e1fb7
Kost911 <Powerofthekee@mail.ru>
2020-02-12 19:42:20 +0300

    3

---
+++ src/Home/work/Test.java
@@ -0,0 +1,15 @@
+package Home.work;
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        Scanner A = new Scanner(System.in);
+        Integer a = A.nextInt();
+        Integer b = 127;
+        Integer c = 127;
+        Integer d = 127;
+        System.out.println(a==b);
+        System.out.println(c==d);
+
+    }
+
+}
