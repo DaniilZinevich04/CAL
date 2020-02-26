d21bd36ce5147ddb8a28126ef8f618f99e945e89
Kost911 <Powerofthekee@mail.ru>
2020-02-26 21:13:32 +0300

    3

---
+++ src/Home/Collection/CollectionMain2.java
@@ -0,0 +1,14 @@
+package Home.Collection;
+
+import Massenger.Person;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionMain2 {
+    public static void main(String[] args) {
+        Map<String,Person> bankData = new HashMap<>();
+        bankData.put("dasddsa","dsadsasa","dassadsa");
+        System.out.println(bankData);
+    }
+}
